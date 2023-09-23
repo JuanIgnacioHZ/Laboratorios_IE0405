fe7e707adae0651683497ddd564430a8583e31bb
JuanIgnacioHZ <juanignaciohz@hotmail.es>
2023-09-23 17:30:17 -0600

    Se añade el cálculo de las frecuencias relativas

--- Laboratorio_1/lab_1_scripts.py
+++ Laboratorio_1/lab_1_scripts.py
@@ -1,6 +1,6 @@
 #!/home/juan/anaconda3/bin/python3.11
 '''
-    Scripts para el análisis estadístico de los datos para el proyecto de IE0405
+    Scripts para análisis estadístico de los datos para el proyecto de IE0405
 '''
 
 # Importación de los datos
@@ -10,26 +10,67 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.rcParams['svg.fonttype'] = 'none'
 
+
+# Declaración de funciones
+def agregar_volumen_categorico(dataframe):
+    '''
+        Esta función se encarga de categorizar los valores de volumen y
+    agregarlos como una columna extra al data frame que se ingresa
+    '''
+    valores_vol = []    # Valores vol = loudness values
+
+    for index, row in dataframe.iterrows():
+        if 0 <= row["loudness"] <= 0.333:
+            valores_vol.append("low")
+        elif 0.333 < row["loudness"] < 0.666:
+            valores_vol.append("mid")
+        else:
+            valores_vol.append("high")
+
+    categorical_loudness = pd.DataFrame({"categorical_loudness": valores_vol})
+
+
+    #print(dataframe["loudness"].between(0, 0.333, inclusive="both").value_counts())
+    #print(dataframe["loudness"].between(0.333, 0.666, inclusive="neither").value_counts())
+    #print(dataframe["loudness"].between(0.666, 1, inclusive="both").value_counts())
+
+    dataframe = dataframe.join(categorical_loudness)
+
+    return dataframe
+
+
+def obtener_frecuencias_relativas(dataframe):
+    '''
+        Se encarga de obtener las frecuencias relativas para las variables
+    categóricas en estudio
+    '''
+
+    # Cantidad total de canciones
+    cant_canciones = len(dataframe.index)
+
+    # Columnas a estudiar
+    variables_categoricas = ['categorical_loudness', 'genre', 'topic']
+
+    # Variable de salida
+    frecuencias = {}
+
+    # Se recorren las variables categóricas para obtener su frecuencia relativa
+    for i in variables_categoricas:
+        dict_tmp = {}   # Variable temporal
+        # Luego, se recorre la cantidad de elementos de cada columna para
+        # obtener su frecuencia relativa
+        for j, k in dataframe[i].value_counts().items():
+            # El formato de los datos es el siguiente:
+            # "nombre de categoría" = frecuencia relativa
+            dict_tmp[j] = k/cant_canciones
+        frecuencias[i] = dict_tmp
+
+    return frecuencias
+
+
 # Importanción del archivo .csv
-musica = pd.read_csv('tcc_ceds_music.csv')
+musica = agregar_volumen_categorico(pd.read_csv('tcc_ceds_music.csv'))
 
-valores_loud = []
+# Declaración de variables
+frec_relativas = obtener_frecuencias_relativas(musica)
 
-for index, row in musica.iterrows():
-    if (0 <= row["loudness"] <= 0.333):
-        valores_loud.append("low")
-    elif (0.333 < row["loudness"] < 0.666):
-        valores_loud.append("mid")
-    else:
-        valores_loud.append("high")
-
-categorical_loudness = pd.DataFrame({"categorical_loudness": valores_loud})
-
-
-#print(musica["loudness"].between(0, 0.333, inclusive="both").value_counts())
-#print(musica["loudness"].between(0.333, 0.666, inclusive="neither").value_counts())
-#print(musica["loudness"].between(0.666, 1, inclusive="both").value_counts())
-
-musica = musica.join(categorical_loudness)
-
-print(musica["categorical_loudness"])
